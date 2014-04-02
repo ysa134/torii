45547c930420b64056ffea69a45ff4576df58b34
Matthew Beale <matt.beale@madhatted.com>
2014-04-02 10:57:30 -0400

    Basic example page

---
+++ example/app.js
@@ -0,0 +1,20 @@
+// If the user does not have a module system, how are
+// we doing this?
+require("torii/ember");
+
+var App = Ember.Application.create();
+
+App.ApplicationRoute = Ember.Route.extend({
+  actions: {
+    authenticate: function(endpoint){
+      var controller = this.controller;
+      controller.set('error', null);
+      controller.set('authData', null);
+      this.get('torii').open(endpoint).then(function(authData){
+        controller.set('authData', authData);
+      }, function(error){
+        controller.set('error', error);
+      });
+    }
+  }
+});
