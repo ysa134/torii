d38c39be20576cc0787200b94a491971b684781d
Matthew Beale <matt.beale@madhatted.com>
2014-04-07 11:13:40 -0400

    Test the oauth2 base endpoint

---
+++ test/tests/integration/endpoints/facebook-oauth2-test.js
@@ -0,0 +1,37 @@
+var torii, container;
+
+import toriiContainer from 'test/helpers/torii-container';
+import configuration from 'torii/configuration';
+
+var originalConfiguration = configuration.endpoints['facebook-oauth2'];
+
+var opened, mockPopup = {
+  open: function(){
+    opened = true;
+    return Ember.RSVP.resolve({});
+  }
+};
+
+module('Facebook - Integration', {
+  setup: function(){
+    container = toriiContainer();
+    container.register('torii-service:mock-popup', mockPopup, {instantiate: false});
+    container.injection('torii-endpoint', 'popup', 'torii-service:mock-popup');
+
+    torii = container.lookup("torii:main");
+    configuration.endpoints['facebook-oauth2'] = {apiKey: 'dummy'};
+  },
+  teardown: function(){
+    opened = false;
+    configuration.endpoints['facebook-oauth2'] = originalConfiguration;
+    Ember.run(container, 'destroy');
+  }
+});
+
+test("Opens a popup to Facebook", function(){
+  Ember.run(function(){
+    torii.open('facebook-oauth2').finally(function(){
+      ok(opened, "Popup service is opened");
+    });
+  });
+});
