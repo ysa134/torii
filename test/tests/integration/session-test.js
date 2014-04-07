1ecc0aa889b50a6aff3548db5638d5cde5ccc503
Matthew Beale <matt.beale@madhatted.com>
2014-04-07 12:24:37 -0400

    Add close session logic

--- test/tests/integration/session-test.js
+++ test/tests/integration/session-test.js
@@ -1,9 +1,9 @@
-var container, session;
+var container, session, user, adapter;
 
 import toriiContainer from 'test/helpers/torii-container';
 import Session from 'torii/session';
 
-module('Session - Integration', {
+module('Session (open) - Integration', {
   setup: function(){
     container = toriiContainer();
     container.register('torii:session', Session);
@@ -55,6 +55,77 @@ test("failed auth sets isAuthenticated to false, sets error", function(){
       ok(!session.get('isOpening'), 'session is no longer opening');
       ok(!session.get('isAuthenticated'), 'session is not authenticated');
       ok(session.get('errorMessage'), 'session has error message');
+    });
+  });
+});
+
+module('Session (close) - Integration', {
+  setup: function(){
+    container = toriiContainer();
+    container.register('torii:session', Session);
+    container.injection('torii:session', 'torii', 'torii:main');
+    session = container.lookup('torii:session');
+    adapter = container.lookup('torii-adapter:application');
+
+    // Put the session in an open state
+    user = {email: 'fake@fake.com'};
+    session.get('stateMachine').transitionTo('opening');
+    session.get('stateMachine').send('finishOpen', user);
+  },
+  teardown: function(){
+    Ember.run(container, 'destroy');
+  }
+});
+
+test("session starts in authenticated opened state", function(){
+  ok(session.get('isAuthenticated'), 'not authenticated');
+  deepEqual(session.get('currentUser'), user, 'has currentUser');
+});
+
+test("starting close sets isWorking to true", function(){
+  var oldClose = adapter.close;
+
+  adapter.close = function(){
+    ok(true, 'calls adapter.close');
+    ok(session.get('isWorking'), 'session.isWorking is true');
+    return Ember.RSVP.resolve();
+  }
+
+  Ember.run(function(){
+    session.close();
+  });
+});
+
+test("finished close sets isWorking to false, isAuthenticated false", function(){
+  adapter.close = function(){
+    return Ember.RSVP.resolve();
+  }
+
+  Ember.run(function(){
+    session.close().then(function(){
+      ok(!session.get('isWorking'), "isWorking is false");
+      ok(!session.get('isAuthenticated'), "isAuthenticated is false");
+      ok(!session.get('currentUser'), "currentUser is false");
+    }, function(err){
+      ok(false, "promise rejected with error: "+err);
+    });
+  });
+});
+
+test("failed close sets isWorking to false, isAuthenticated true, error", function(){
+  var error = 'Oh my';
+
+  adapter.close = function(){
+    return Ember.RSVP.reject(error);
+  }
+
+  Ember.run(function(){
+    session.close().then(function(){
+      ok(false, "promise resolved");
+    },function(error){
+      ok(!session.get('isWorking'), "isWorking is false");
+      ok(!session.get('isAuthenticated'), "isAuthenticated is true");
+      equal(session.get('errorMessage'), error, "error is present");
     });
   });
 });
