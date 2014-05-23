be7c91bfb7935b3493ec8487b674553d1a5b170d
Matthew Beale <matt.beale@madhatted.com>
2014-05-23 17:55:39 -0400

    Poll for a closed popup. Fixes #7, Refs #8. Fixes #14
    Move to promises, and poll for a window closed by the user. Improve the
    tests around popups.

--- test/tests/unit/services/popup-test.js
+++ test/tests/unit/services/popup-test.js
@@ -33,10 +33,73 @@ asyncTest("open resolves based on popup window", function(){
     popup.open(expectedUrl, ['code']).then(function(data){
       ok(true, 'resolves promise');
       deepEqual(data, {code: 'fr'}, 'resolves with expected data');
-
+      start();
+    }, function(){
+      ok(false, 'rejected the open promise');
       start();
     });
   });
 
   window.postMessage(expectedData, '*');
+});
+
+asyncTest("open rejects when window does not open", function(){
+  var closedWindow = Ember.copy(mockWindow);
+  closedWindow.closed = true;
+  window.open = function(url){
+    ok(true, 'calls window.open');
+    return closedWindow;
+  };
+
+  Ember.run(function(){
+    popup.open('http://some-url.com', ['code']).then(function(data){
+      ok(false, 'resolves promise');
+      start();
+    }, function(){
+      ok(true, 'rejected the open promise');
+      start();
+    });
+  });
+});
+
+asyncTest("open does not resolve when recieving the wrong message", function(){
+  window.open = function(url){
+    ok(true, 'calls window.open');
+    return mockWindow;
+  };
+
+  var promise = Ember.run(function(){
+    return popup.open('http://someserver.com', ['code']).then(function(data){
+      ok(false, 'resolves the open promise');
+      start();
+    }, function(){
+      ok(false, 'rejected the open promise');
+      start();
+    });
+  });
+
+  window.postMessage('somenotvaliddata', '*');
+  setTimeout(function(){
+    ok(!promise.isFulfilled, 'promise is not fulfulled by invalid data');
+    start();
+  },10);
+});
+
+asyncTest("open rejects when window closes", function(){
+  window.open = function(){
+    ok(true, 'calls window.open');
+    return mockWindow;
+  };
+
+  Ember.run(function(){
+    popup.open('some-url', ['code']).then(function(){
+      ok(false, 'resolved the open promise');
+      start();
+    }, function(){
+      ok(true, 'rejected the open promise');
+      start();
+    });
+  });
+
+  mockWindow.closed = true;
 });
