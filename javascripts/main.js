ae8328e8818f2db80dc0ef3a8e3607e964de30bf
Matthew Beale <matt.beale@madhatted.com>
2014-06-13 10:25:36 -0400

    Update the site as provided by Arthur

---
+++ javascripts/main.js
@@ -0,0 +1,9 @@
+;(function($, window, document, undefined) {
+	var $win = $(window);
+	var $doc = $(document);
+
+	$doc.ready(function() {
+		// fullscreener
+		$('.intro > img').fullscreener();
+	});
+})(jQuery, window, document);
