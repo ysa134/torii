9e7d5f12241866a8484b60b2fed8af3670f4938d
Matthew Beale <matt.beale@madhatted.com>
2014-09-17 12:16:54 -0400

    Fix iOS popup closing

--- test/tests/unit/redirect-handler-test.js
+++ test/tests/unit/redirect-handler-test.js
@@ -1,6 +1,7 @@
 import RedirectHandler from 'torii/redirect-handler';
 
 var originalPostMessage = window.postMessage;
+var originalClose = window.close;
 
 module('RedirectHandler - Unit', {
   setup: function(){
@@ -9,10 +10,13 @@ module('RedirectHandler - Unit', {
       name: 'torii-opener',
       postMessage: Ember.K
     };
+    window.close = Ember.K;
   },
   teardown: function(){
     window.name = null;
     window.opener = null;
+    window.postMessage = originalPostMessage;
+    window.close = originalClose;
   }
 });
 
@@ -68,6 +72,24 @@ test('does not post a message', function(){
   });
 });
 
+test('closes the window on no data in url', function(){
+  expect(1);
+  var url = "http://authServer";
+  var handler = new RedirectHandler(url);
+
+  window.close = function(){
+    ok(true, "Window was closed");
+  };
+
+  Ember.run(function(){
+    handler.run().then(function(){
+      ok(false, "run handler succeeded on a popup");
+    }, function(error){
+      ok(false, "run handler rejects a popup without a name (should hang)");
+    });
+  });
+});
+
 test('posts a message', function(){
   var code = "d29f2jf20j",
       url = "http://authServer?code="+code,
@@ -78,6 +100,20 @@ test('posts a message', function(){
     postMessage: function(message, origin){
       equal(message, "__torii_message:"+url, "posts back the url");
     }
+  };
+
+  Ember.run(function(){
+    handler.run();
+  });
+});
+
+test('closes the window', function(){
+  var code = "d29f2jf20j",
+      url = "http://authServer?code="+code,
+      handler = new RedirectHandler(url);
+
+  window.close = function(){
+    ok(true, "Window was closed");
   };
 
   Ember.run(function(){
