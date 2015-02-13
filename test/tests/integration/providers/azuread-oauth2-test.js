29486d1f5e0cf2038d3f3206e42d7bc2e242ab74
Albert-Jan Nijburg <albertjan@curit.com>
2015-02-13 12:24:39 +0100

    add azuread oauth2 provider

---
+++ test/tests/integration/providers/azuread-oauth2-test.js
@@ -0,0 +1,37 @@
+var torii, container;
+
+import toriiContainer from 'test/helpers/torii-container';
+import configuration from 'torii/configuration';
+
+var originalConfiguration = configuration.providers['azuread-oauth2'];
+
+var opened, mockPopup = {
+  open: function(){
+    opened = true;
+    return Ember.RSVP.resolve({});
+  }
+};
+
+module('AzureAd - Integration', {
+  setup: function(){
+    container = toriiContainer();
+    container.register('torii-service:mock-popup', mockPopup, {instantiate: false});
+    container.injection('torii-provider', 'popup', 'torii-service:mock-popup');
+
+    torii = container.lookup("torii:main");
+    configuration.providers['azuread-oauth2'] = { cliendId: 'dummy' };
+  },
+  teardown: function(){
+    opened = false;
+    configuration.providers['azuread-oauth2'] = originalConfiguration;
+    Ember.run(container, 'destroy');
+  }
+});
+
+test("Opens a popup to AzureAd", function(){
+  Ember.run(function(){
+    torii.open('azuread-oauth2').finally(function(){
+      ok(opened, "Popup service is opened");
+    });
+  });
+});
