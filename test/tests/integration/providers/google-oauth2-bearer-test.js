75287c7e3c4d26653627b08e298e72daac19895b
Lin Reid <linreid@gmail.com>
2015-04-05 07:52:11 -0400

    Add google-oauth2-bearer provider

---
+++ test/tests/integration/providers/google-oauth2-bearer-test.js
@@ -0,0 +1,53 @@
+var torii, container;
+
+import toriiContainer from 'test/helpers/torii-container';
+import configuration from 'torii/configuration';
+
+var originalConfiguration = configuration.providers['google-oauth2-bearer'];
+
+var opened, mockPopup;
+
+module('Google Bearer- Integration', {
+  setup: function(){
+    mockPopup = {
+      open: function(){
+        opened = true;
+        return Ember.RSVP.resolve({ access_token: 'test' });
+      }
+    };
+    container = toriiContainer();
+    container.register('torii-service:mock-popup', mockPopup, {instantiate: false});
+    container.injection('torii-provider', 'popup', 'torii-service:mock-popup');
+
+    torii = container.lookup("torii:main");
+    configuration.providers['google-oauth2-bearer'] = {apiKey: 'dummy'};
+  },
+  teardown: function(){
+    opened = false;
+    configuration.providers['google-oauth2-bearer'] = originalConfiguration;
+    Ember.run(container, 'destroy');
+  }
+});
+
+test("Opens a popup to Google", function(){
+  expect(1);
+  Ember.run(function(){
+    torii.open('google-oauth2-bearer').finally(function(){
+      ok(opened, "Popup service is opened");
+    });
+  });
+});
+
+test("Opens a popup to Google with request_visible_actions", function(){
+  expect(1);
+  configuration.providers['google-oauth2-bearer'].requestVisibleActions = "http://some-url.com";
+  mockPopup.open = function(url){
+    ok(
+      url.indexOf("request_visible_actions=http%3A%2F%2Fsome-url.com") > -1,
+      "request_visible_actions is present" );
+    return Ember.RSVP.resolve({ access_token: 'test' });
+  }
+  Ember.run(function(){
+    torii.open('google-oauth2-bearer');
+  });
+});
---
+++ test/tests/unit/providers/google-oauth2-bearer-test.js
@@ -0,0 +1,39 @@
+var provider;
+
+import configuration from 'torii/configuration';
+
+import GoogleBearerProvider from 'torii/providers/google-oauth2-bearer';
+
+module('Unit - GoogleAuth2BearerProvider', {
+  setup: function(){
+    configuration.providers['google-oauth2-bearer'] = {};
+    provider = new GoogleBearerProvider();
+  },
+  teardown: function(){
+    Ember.run(provider, 'destroy');
+    configuration.providers['google-oauth2-bearer'] = {};
+  }
+});
+
+test("Provider requires an apiKey", function(){
+  configuration.providers['google-oauth2-bearer'] = {};
+  throws(function(){
+    provider.buildUrl();
+  }, /Expected configuration value providers.google-oauth2-bearer.apiKey to be defined!/);
+});
+
+test("Provider generates a URL with required config", function(){
+  configuration.providers['google-oauth2-bearer'] = {
+    apiKey: 'abcdef'
+  };
+
+  var expectedUrl = provider.get('baseUrl') + '?' + 'response_type=token' +
+          '&client_id=' + 'abcdef' +
+          '&redirect_uri=' + encodeURIComponent(provider.get('redirectUri')) +
+          '&state=STATE' +
+          '&scope=email';
+
+  equal(provider.buildUrl(),
+        expectedUrl,
+        'generates the correct URL');
+});
