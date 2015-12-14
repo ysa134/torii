5d6710204425bca0269c2b6dbf5c40e9aa349d45
Matthew Beale <matt.beale@madhatted.com>
2015-12-14 12:04:21 -0500

    Lazily look up the popup type

--- test/tests/unit/bootstrap/torii-test.js
+++ test/tests/unit/bootstrap/torii-test.js
@@ -1,21 +1,59 @@
 import toriiContainer from 'test/helpers/torii-container';
+import BaseProvider from 'torii/providers/base';
+import IframeService from 'torii/services/iframe';
+import PopupService from 'torii/services/popup';
+import configuration from 'torii/configuration';
 
-var container, registry;
+var container, registry, FooProvider;
 
 module("boostrapTorii", {
-  teardown: function(){
+  setup: function() {
+    FooProvider = BaseProvider.extend({
+      name: 'foo'
+    });
+  },
+  teardown: function() {
     Ember.run(container, 'destroy');
     registry = container = null;
+    FooProvider = null;
     window.DS = null;
+    delete configuration.remoteServiceName;
+    delete configuration.providers.foo;
   }
 });
 
-test("inject popup onto providers", function(){
+test("default popup on providers", function(){
   var results = toriiContainer();
   registry = results[0];
   container = results[1];
-  registry.register('torii-provider:foo', Ember.Object.extend());
-  ok(container.lookup('torii-provider:foo').get('popup'), 'Popup is set');
+  registry.register('torii-provider:foo', FooProvider);
+  var popup = container.lookup('torii-provider:foo').get('popup');
+  ok(popup, 'Popup is set');
+  ok(popup instanceof PopupService, 'Popup service is popup');
+});
+
+test("configured default popup", function(){
+  configuration.remoteServiceName = 'iframe';
+  var results = toriiContainer();
+  registry = results[0];
+  container = results[1];
+  registry.register('torii-provider:foo', FooProvider);
+  var popup = container.lookup('torii-provider:foo').get('popup');
+  ok(popup, 'Popup is set');
+  ok(popup instanceof IframeService, 'Popup service is iframe');
+});
+
+test("configured popup on providers", function(){
+  configuration.providers.foo = {
+    remoteServiceName: 'iframe'
+  };
+  var results = toriiContainer();
+  registry = results[0];
+  container = results[1];
+  registry.register('torii-provider:foo', FooProvider);
+  var popup = container.lookup('torii-provider:foo').get('popup');
+  ok(popup, 'Popup is set');
+  ok(popup instanceof IframeService, 'Popup service is iframe');
 });
 
 test("inject legacy DS store onto providers, adapters", function(){
@@ -27,7 +65,7 @@ test("inject legacy DS store onto providers, adapters", function(){
   registry = results[0];
   container = results[1];
 
-  registry.register('torii-provider:foo', Ember.Object.extend());
+  registry.register('torii-provider:foo', FooProvider);
   var provider = container.lookup('torii-provider:foo');
   ok(provider.get('store'), 'Store is set on providers');
 
@@ -45,7 +83,7 @@ test("inject DS store onto providers, adapters", function(){
   registry = results[0];
   container = results[1];
 
-  registry.register('torii-provider:foo', Ember.Object.extend());
+  registry.register('torii-provider:foo', FooProvider);
   var provider = container.lookup('torii-provider:foo');
   ok(provider.get('store'), 'Store is set on providers');
 
